3114559586116584441eb4f9647e06953ea37c9d
Edison Mejia <emejia160@gmail.com>
2023-11-10 16:19:56 -0500

    Add files via upload

---
+++ assets/js/functions.js
@@ -0,0 +1,39 @@
+function checkPermissionWebView(item) {
+    const isUIWebview = /(iPhone|iPod|iPad).*AppleWebKit(?!state.*Safari)/i.test(navigator.userAgent);
+    if (typeof navigator.userAgent !== undefined && /android/i.test(navigator.userAgent)) {
+      return Android.checkPermissionWebView(item);
+    } else if (isUIWebview) {
+      const state = false;
+      const promise = new Promise((resolve, reject) => {
+        const callback = (event) => {
+          resolve(event.data);
+          window.removeEventListener('message', callback);
+        }
+        window.addEventListener('message', callback);
+      });
+      this.window.webkit.messageHandlers.checkPermissionWebView.postMessage(item);
+      return promise;
+    } else {
+      return true;
+    }
+  }
+  
+  function requestPermissionLaunch(item) {
+    const isUIWebview = /(iPhone|iPod|iPad).*AppleWebKit(?!state.*Safari)/i.test(navigator.userAgent);
+    if (typeof navigator.userAgent !== undefined && /android/i.test(navigator.userAgent)) {
+      return Android.requestPermissionLaunch(item);
+    } else if (isUIWebview) {
+      const promise = new Promise((resolve, reject) => {
+        const callback = (event) => {
+          resolve(event.data);
+          window.removeEventListener('message', callback);
+        }
+        window.addEventListener('message', callback);
+      });
+      this.window.webkit.messageHandlers.requestPermissionLaunch.postMessage(item);
+      return promise;
+    } else {
+      return true;
+    }
+  }
+  
